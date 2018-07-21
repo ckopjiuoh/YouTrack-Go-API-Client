1f231805131402f671bf053516b3913b8a3d9314
Ilya Novikov <iliya.a.novikov@gmail.com>
2018-07-21 15:11:59 +0300

    add interface

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,13 +1,13 @@
 package helpers
 
 import (
-	"net/http"
 	"io/ioutil"
+	"net/http"
 )
 
 func ReadBody(r *http.Response) ([]byte, error) {
 	bodyBytes, err := ioutil.ReadAll(r.Body)
-	r.Body.Close()  //  must close
+	r.Body.Close() //  must close
 
 	return bodyBytes, err
 }
--- issue.go
+++ issue.go
@@ -1,19 +1,24 @@
 package ytapi
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/url"
-	"github.com/ckopjiuoh/YouTrack-Go-API-Client/helpers"
-	"encoding/json"
+
+	"strings"
+
+	"strconv"
+
+	"github.com/ckopjiuoh/go-youtrack-api-client/helpers"
 )
 
 type Issue struct {
 	ID       string        `json:"id"`
 	EntityID string        `json:"entityId"`
-	JiraID   interface{}   `json:"jiraId"`
-	Field    []*Field       `json:"field"`
+	JiraID   string        `json:"jiraId, omitempty"`
+	Field    []Field       `json:"field"`
 	Comment  []interface{} `json:"comment"`
-	Tag      []*Tag         `json:"tag"`
+	Tag      []Tag         `json:"tag"`
 }
 
 type Tag struct {
@@ -27,10 +32,61 @@ type Color struct {
 }
 
 type Field struct {
-	Name    string   `json:"name"`
-	Value   string   `json:"value"`
-	ValueID []string `json:"valueId,omitempty"`
-	Color   Color    `json:"color,omitempty"`
+	Name    string      `json:"name"`
+	Value   interface{} `json:"value"`
+	ValueID []string    `json:"valueId,omitempty"`
+	Color   Color       `json:"color,omitempty"`
+}
+
+func (is *Issue) GetValueByFieldName(name string) (interface{}, error) {
+	for _, v := range is.Field {
+		if strings.ToLower(v.Name) == strings.ToLower(name) {
+			return v.Value, nil
+		}
+	}
+	return nil, fmt.Errorf("can't find field: %s", name)
+}
+
+func (is *Issue) GetID() string {
+	return is.ID
+}
+
+func (is *Issue) GetEntityID() string {
+	return is.EntityID
+}
+
+func (is *Issue) GetProjectShortName() (string, error) {
+	p, err := is.GetValueByFieldName("ProjectShortName")
+	if err != nil {
+		return "", err
+	}
+	return p.(string), nil
+}
+
+func (is *Issue) GetNumberInProject() (int, error) {
+	p, err := is.GetValueByFieldName("NumberInProject")
+	if err != nil {
+		return -1, err
+	}
+
+	d, err := strconv.Atoi(p.(string))
+	return d, err
+}
+
+func (is *Issue) GetSummary() (string, error) {
+	p, err := is.GetValueByFieldName("ValueByFieldName")
+	if err != nil {
+		return "", err
+	}
+	return p.(string), nil
+}
+
+func (is *Issue) GetDescription() (string, error) {
+	p, err := is.GetValueByFieldName("Description")
+	if err != nil {
+		return "", err
+	}
+	return p.(string), nil
 }
 
 func (api *YouTrackAPI) GetAnIssue(name string) (*Issue, error) {
@@ -58,13 +114,14 @@ func (api *YouTrackAPI) GetAnIssue(name string) (*Issue, error) {
 		return nil, e
 	}
 
-	issue := &Issue{}
+	issue := Issue{}
 
-	e = json.Unmarshal(b, issue)
+	e = json.Unmarshal(b, &issue)
 
 	if e != nil {
+		println(string(b))
 		return nil, e
 	}
 
-	return issue, nil
+	return &issue, nil
 }
